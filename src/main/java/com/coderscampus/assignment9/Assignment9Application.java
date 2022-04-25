10d06a1303da7a5ef77ef05f614c45c7d21887c4
tristandanle <letristandan@gmail.com>
2022-04-24 23:59:47 -0400

    Update Assignment9Application.java

--- src/main/java/com/coderscampus/assignment9/Assignment9Application.java
+++ src/main/java/com/coderscampus/assignment9/Assignment9Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("com.coderscampus.assignment9")
+#@ComponentScan("com.coderscampus.assignment9")
 public class Assignment9Application {
 
 	public static void main(String[] args) {
